4f83768227ea5ae007c507de7ef2213b45306b84
Lugeja <aaronalev@gmail.com>
2024-06-13 23:59:54 +0300

    Fix existing user and password limitation errors

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,25 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
+const { where } = require('sequelize');
 
-const register = (req, res) => {
-    console.log(req.body);
+const register = async (req, res) => {
+    try {
+        const user = await User.findOne({ where: { username: req.body.username } });
+        if (user) {
+            return res.status(400).json({
+                error: 'account with username already exists'
+            });
+        } else if (req.body.password.length < 6) {
+            return res.status(400).json({
+                error: 'password must be at least 6 characters long'
+            });
+        }
+    } catch (error) {
+        return res.status(500).json({
+            error: error
+        });
+    }
+
     bcrypt.hash(req.body.password, 10, (error, cryptPassword) => {
         User.create({
             username: req.body.username,
